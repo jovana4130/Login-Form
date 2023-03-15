ec7dac32170694ffaef75bbcacaff8bd3c1de3d0
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-15 18:19:10 +0100

    CSS setup

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,7 +4,7 @@ import './MainContent.css';
 
 
 return (
-    <div className='container'>
+    <div className='main-container'>
         <form>
             <h1>LOGIN</h1>
             <input></input>
